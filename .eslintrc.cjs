a407e95318df583694210e185684957a6848f805
Sylvain Jouglard <sylvain.jouglard@cdbdx.biz>
2023-07-19 17:06:44 +0200

    add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,31 +3,30 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
     "plugin:import/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:testing-library/react",
     "plugin:security/recommended",
     "plugin:xss/recommended",
     "prettier",
-
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
   overrides: [
     {
-      files: ["*.js", "*.jsx"]
-    }
+      files: ["*.js", "*.jsx"],
+    },
   ],
   ignorePatterns: ["dist", "storybook-static", "coverage"],
-  plugins: ['react-refresh', "simple-import-sort"],
+  plugins: ["react-refresh", "simple-import-sort"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
