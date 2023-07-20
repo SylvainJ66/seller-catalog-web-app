b35712bfb52f1a20b884a602c48ecb1366c1b94b
Sylvain Jouglard <sylvain.jouglard@cdiscount.com>
2023-07-20 16:48:00 +0200

    feat: 🎸 add clsx

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,28 +1,37 @@
 import "./header.css";
 import { useDarkLightMode } from "@/hooks";
+import clsx from "clsx";
 
 export default function Header() {
   const [currentMode, { toggleMode }] = useDarkLightMode();
 
   return (
     <>
-      <header>
-        <h1 className="logo">My React App</h1>
+      <header className={clsx({ light: currentMode })}>
+        <h1 className={clsx("logo", { light: currentMode })}>My React App</h1>
         <nav>
           <ol className="navList">
             <li className="navListItem">
-              <a href="/page1" className="navLink">
+              <a
+                href="/page1"
+                className={clsx("navLink", { light: currentMode })}
+              >
                 Page 1
               </a>
             </li>
             <li>
-              <a href="/page2" className="navLink">
+              <a
+                href="/page2"
+                className={clsx("navLink", { light: currentMode })}
+              >
                 Page 2
               </a>
             </li>
           </ol>
         </nav>
-        <button onClick={toggleMode}>Mode {currentMode}</button>
+        <button onClick={toggleMode}>
+          Mode {currentMode ? "Dark" : "Light"}
+        </button>
       </header>
     </>
   );
--- src/hooks/useDarkLightMode.js
+++ src/hooks/useDarkLightMode.js
@@ -2,6 +2,5 @@ import { useToggle } from "react-use";
 
 export default function useDarkLightMode() {
   const [currentMode, toggleMode] = useToggle(true);
-  const currentModeText = currentMode ? "Light" : "Dark";
-  return [currentModeText, { toggleMode }];
+  return [currentMode, { toggleMode }];
 }
