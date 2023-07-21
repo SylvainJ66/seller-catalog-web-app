1dc3215eb912fcc95c33c1f0ef4971df736a64e2
Sylvain Jouglard <sylvain.jouglard@cdiscount.com>
2023-07-21 13:43:54 +0200

    feat: 🎸 remove 404 on image

--- src/Template/index.jsx
+++ src/Template/index.jsx
@@ -1,9 +1,72 @@
 import Header from "@/Header/index.js";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+
+const mainMenu = [
+  { name: "Produits", url: "/" },
+  { name: "Commandes", url: "/orders" },
+  { name: "Finance", url: "/financial" },
+  { name: "Messages", url: "/messages" },
+  { name: "Marketplaces", url: "/marketPlaces" },
+];
 
 export default function Template() {
   return (
     <>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component={Link}
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              Seller Catalog
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {mainMenu.map(({ name, url }) => (
+                <Button
+                  key={url}
+                  component={Link}
+                  to={url}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {name}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={() => {}} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="sergio.jpg" />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
       <Header />
       <Outlet />
     </>
